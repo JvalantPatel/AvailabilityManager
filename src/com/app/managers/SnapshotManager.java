8825ccaacf54646993b1dba0b09bafd37939b4c1
rainashastri30 <rainashastri30@gmail.com>
2015-04-01 08:57:44 +0530

    Adding code for SnapshotManager

--- src/com/app/managers/SnapshotManager.java
+++ src/com/app/managers/SnapshotManager.java
@@ -1,5 +1,29 @@
 package com.app.managers;
 
-public class SnapshotManager extends Thread{
+import com.app.data.InfrastructureData;
+import com.app.handlers.SnapshotHandler;
+
+public class SnapshotManager extends Thread {
+	SnapshotHandler snapShothandler;
+
+	public SnapshotManager() {
+
+		snapShothandler = new SnapshotHandler();
+
+	}
+
+	public void run() {
+
+		// create snapshot for vm
+
+		snapShothandler.createSnapShotForVM(InfrastructureData.getInstance());
+
+		// create snapshot for host
+		/*
+		 * snapShothandler.createSnapShotForHost(InfrastructureData
+		 * .getInstance());
+		 */
+
+	}
 
 }
